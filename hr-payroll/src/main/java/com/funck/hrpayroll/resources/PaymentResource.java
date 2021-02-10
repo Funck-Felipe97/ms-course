fcbce527dc2abacd59700ff47bd4b0f7169577f3
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2021-02-10 20:24:28 -0300

    Configure netflix hystrix

--- hr-payroll/src/main/java/com/funck/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/funck/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.funck.hrpayroll.resources;
 
 import com.funck.hrpayroll.entities.Payment;
 import com.funck.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentResource {
 
     private final PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/worker/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.00, days);
+        return ResponseEntity.ok(payment);
     }
 
 }
