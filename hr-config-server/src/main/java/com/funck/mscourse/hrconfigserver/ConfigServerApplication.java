d535298dbc921430858bc3063db93effd90275a8
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2021-02-13 19:51:53 -0300

    Config hr-config-server

--- hr-config-server/src/main/java/com/funck/mscourse/hrconfigserver/ConfigServerApplication.java
+++ hr-config-server/src/main/java/com/funck/mscourse/hrconfigserver/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.funck.mscourse.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HrConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
