11c7189d1cb24fec14657c46aa27a205e50a4cd7
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2021-02-15 19:56:17 -0300

    Configure config client

---
+++ hr-worker/src/main/java/com/funck/hrworker/resources/ConfigPropertiesResource.java
@@ -0,0 +1,20 @@
+package com.funck.hrworker.resources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("workers/configs")
+public class ConfigPropertiesResource {
+
+    @Value("${test.config}")
+    private String config;
+
+    @GetMapping
+    public String test() {
+        return config;
+    }
+
+}
